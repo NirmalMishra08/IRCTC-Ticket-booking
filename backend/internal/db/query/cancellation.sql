c466076697e01dfaa8965af85f7e7e06c91e3ef1
NirmalMishra08 <145590866+NirmalMishra08@users.noreply.github.com>
2026-01-05 22:23:04 +0530

    added sqlc function

---
+++ backend/internal/db/query/cancellation.sql
@@ -0,0 +1,6 @@
+
+-- name: GetPaymentAndTrain :one
+SELECT p.amount,p.status as payment_status,b.travelDate , b.status as booking_status FROM
+booking b JOIN
+payment p ON b.id = p.bookingId
+WHERE b.trainId = $2 AND b.userId = $1;
