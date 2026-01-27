26346969e9c701cacee9d4019a09275c02e82e11
NirmalMishra08 <145590866+NirmalMishra08@users.noreply.github.com>
2026-01-27 23:55:52 +0530

    working on tatkal

---
+++ backend/internal/api/tatkal/handler.go
@@ -0,0 +1,33 @@
+package tatkal
+
+import (
+	"better-uptime/common/middleware"
+	"better-uptime/common/routes"
+	"better-uptime/config"
+	db "better-uptime/internal/db/sqlc"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type Handler struct {
+	store  db.Store
+	config *config.Config
+}
+
+func NewHandler(config *config.Config, store db.Store) *Handler {
+	return &Handler{
+		config: config,
+		store:  store,
+	}
+}
+
+func (h *Handler) Routes() *chi.Mux {
+	router := routes.DefaultRouter()
+
+	router.Group(func(r chi.Router) {
+		r.Use(middleware.TokenMiddleware(h.store))
+
+	})
+
+	return router
+}
