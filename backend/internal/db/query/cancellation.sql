9eeed60524fb7a9f20892ff98a650a5c736625eb
NirmalMishra08 <145590866+NirmalMishra08@users.noreply.github.com>
2026-02-19 00:33:18 +0530

    wrote more sqlc functions

--- backend/internal/db/query/cancellation.sql
+++ backend/internal/db/query/cancellation.sql
@@ -1,13 +1,21 @@
 
--- -- name: GetPaymentAndTrain :one
--- SELECT p.amount,p.status as payment_status,b.id as booking_id , b.travelDate , b.holdToken , b.status as booking_status FROM
--- booking b JOIN
--- payment p ON b.id = p.bookingId
--- WHERE b.trainId = $2 AND b.userId = $1 AND b.travelDate = $3
--- LIMIT 1;
+-- name: GetPaymentAndTrain :one
+SELECT b.* , p.*
+FROM
+booking b JOIN
+payment p ON b.id = p.bookingId
+WHERE b.journey_id = $2 AND b.userId = $1 AND p.status = "SUCCESS";
 
--- -- name: CreateRefund :one
--- INSERT INTO refund (userId , bookingId , amount , status , createdAt, updatedAt) 
--- VALUES ( $1 , $2 , $3 , $4 , now() , now() )
--- RETURNING *;
+
+-- name: CreateRefund :one
+INSERT INTO refund (userId , bookingId , amount , status , createdAt, updatedAt) 
+VALUES ( $1 , $2 , $3 , $4 , now() , now() )
+RETURNING *;
+
+-- name: ReleaseSeatsByBooking :exec
+UPDATE seat_inventory
+SET status = 'AVAILABLE',
+    booking_id = NULL
+WHERE booking_id = $1
+  AND status = 'CONFIRMED';
 
